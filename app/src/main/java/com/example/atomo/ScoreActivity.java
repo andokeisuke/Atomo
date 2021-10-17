af60720dc03d1e10016727a807aff2535ea1a1ee
andokeisuke <37164896+andokeisuke@users.noreply.github.com>
2021-10-17 17:18:59 +0900

    add timer progress

--- app/src/main/java/com/example/atomo/ScoreActivity.java
+++ app/src/main/java/com/example/atomo/ScoreActivity.java
@@ -3,17 +3,32 @@ package com.example.atomo;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.text.format.Time;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class ScoreActivity extends AppCompatActivity {
 
-    private int ite = 0;
+
     private MyValue myValue;
+    private Timer timer;
+    private CountUpTimerTask timerTask;
+    private final Handler handler = new Handler(Looper.getMainLooper());
+    private long count;
+    private ImageButton start_Button;
+    private TextView score;
+    private ProgressBar progressBar;
+    private String madorinum;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,51 +36,50 @@ public class ScoreActivity extends AppCompatActivity {
         myValue = (MyValue) this.getApplication();
 
         Intent i = getIntent();
-        String madorinum = i.getStringExtra("madorinum");
+        madorinum = i.getStringExtra("madorinum");
 
         setContentView(R.layout.activity_score);
 
-        ImageButton start_Button = findViewById(R.id.start_button);
+        start_Button = findViewById(R.id.start_button);
         ImageButton reset_Button = findViewById(R.id.reset_button);
-        TextView score = findViewById(R.id.score);
+        score = findViewById(R.id.score);
         start_Button.setImageResource(R.drawable.start_icon);
+
+        progressBar = findViewById(R.id.progressBarToday);
+        progressBar.setProgress(0);
 
 
         //letterへ遷移
         start_Button.setOnClickListener(v -> {
 
-            ite++;
 
+            start_Button.setImageResource(R.drawable.socring_icon);
+            score.setText("");
+            progressBar.setProgress(0);
 
-            if(ite == 0){
-                start_Button.setImageResource(R.drawable.start_icon);
-                score.setText("");
+            // Timer インスタンスを生成
+            timer = new Timer();
 
+            // TimerTask インスタンスを生成
+            timerTask = new CountUpTimerTask();
 
-            }else if(ite == 1){
-                start_Button.setImageResource(R.drawable.socring_icon);
-                score.setText("");
-
-            }else if(ite == 2){
-                start_Button.setImageResource(R.drawable.finish_icon);
-                score.setText("26");
-
-                Time time = new Time("Asia/Tokyo");
-                time.setToNow();
-                String date = time.year + "\n"+ (time.month+1) + "/" + time.monthDay;
-                String[] diagnose = new String[]{date,madorinum,"26"};
-                myValue.addDisagnose(diagnose);
-            }
-
-
+            // スケジュールを設定 100msec
+            // public void schedule (TimerTask task, long delay, long period)
+            timer.schedule(timerTask, 0, 1000);
 
         });
 
         reset_Button.setOnClickListener(view -> {
 
-            ite = 0;
             start_Button.setImageResource(R.drawable.start_icon);
             score.setText("");
+            progressBar.setProgress(0);
+            count = 0;
+            if(null != timer){
+                // Cancel
+                timer.cancel();
+                timer = null;
+            }
         });
 
         ImageButton diagnose_Button = findViewById(R.id.diagnose_button);
@@ -111,5 +125,39 @@ public class ScoreActivity extends AppCompatActivity {
 
 
 
+    }
+
+    class CountUpTimerTask extends TimerTask {
+        @Override
+        public void run() {
+            // handlerを使って処理をキューイングする
+            handler.post(new Runnable() {
+                public void run() {
+                    count++;
+                    progressBar.setProgress((int)(count));
+
+                    if(count == 10){
+                        start_Button.setImageResource(R.drawable.finish_icon);
+                        progressBar.setProgress(0);
+                        score.setText("26");
+
+                        Time time = new Time("Asia/Tokyo");
+                        time.setToNow();
+                        String date = time.year + "\n"+ (time.month+1) + "/" + time.monthDay;
+                        String[] diagnose = new String[]{date,madorinum,"26"};
+                        myValue.addDisagnose(diagnose);
+                        count = 0;
+
+                        if(null != timer){
+                            // Cancel
+                            timer.cancel();
+                            timer = null;
+                        }
+
+                    }
+
+                }
+            });
+        }
     }
 }
